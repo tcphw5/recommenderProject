ba233a6f38c2617f7d95c6723c16c31339a2f407
tcphw5 <tcphw5@mst.edu>
2017-03-22 19:56:58 -0500

    got all reccomender algorithms running and testing CF with different values of k and different measures of similarity

---
+++ recSys.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Reccomender Systems Project first file
+
+Created on Wed Mar 22 11:28:54 2017
+
+@author: tpwin10
+"""
+
+from surprise import Dataset
+from surprise import Reader
+from surprise import SVD
+from surprise import NMF
+from surprise import evaluate, print_perf
+from surprise import KNNBasic
+import os
+
+#load data from file
+fileIN = os.path.expanduser('restaurant_ratings.txt')
+reader = Reader(line_format='user item rating timestamp', sep='\t')
+data = Dataset.load_from_file(fileIN, reader=reader)
+
+data.split(n_folds=3)
+
+"""
+parts 5-12 of HW4.pdf
+
+"""
+algs = [SVD(), SVD(biased=False), NMF(), KNNBasic(sim_options={'user_based': True}), KNNBasic(sim_options={'user_based': False})]
+
+fold1 = {'mae':[], 'rmse':[]}
+fold2 = {'mae':[], 'rmse':[]}
+fold3 = {'mae':[], 'rmse':[]}
+means = {'mae':[], 'rmse':[]}
+
+for alg in algs:
+    perf = evaluate(alg, data, measures=['RMSE', 'MAE'])
+    
+    fold1['mae'].append(perf['mae'][0])
+    fold2['mae'].append(perf['mae'][1])
+    fold3['mae'].append(perf['mae'][2])
+    means['mae'].append(sum(perf['mae'])/len(perf['mae']))
+    fold1['rmse'].append(perf['rmse'][0])
+    fold2['rmse'].append(perf['rmse'][1])
+    fold3['rmse'].append(perf['rmse'][2])
+    means['rmse'].append(sum(perf['rmse'])/len(perf['rmse']))
+    
+    print_perf(perf)
+
+"""
+part 13 of HW4.pdf
+
+"""
+CFalgs = [KNNBasic(sim_options={'user_based': True, 'name':'MSD'}), KNNBasic(sim_options={'user_based': True,'name':'cosine'}), KNNBasic(sim_options={'user_based': True,'name':'pearson'}), KNNBasic(sim_options={'user_based': False,'name':'MSD'}), KNNBasic(sim_options={'user_based': False, 'name':'cosine'}), KNNBasic(sim_options={'user_based': False,'name':'pearson'})]
+
+fold1cf = {'mae':[], 'rmse':[]}
+fold2cf = {'mae':[], 'rmse':[]}
+fold3cf = {'mae':[], 'rmse':[]}
+meanscf = {'mae':[], 'rmse':[]}
+
+for alg in CFalgs:
+    perf = evaluate(alg, data, measures=['RMSE', 'MAE'])
+    
+    fold1cf['mae'].append(perf['mae'][0])
+    fold2cf['mae'].append(perf['mae'][1])
+    fold3cf['mae'].append(perf['mae'][2])
+    meanscf['mae'].append(sum(perf['mae'])/len(perf['mae']))
+    fold1cf['rmse'].append(perf['rmse'][0])
+    fold2cf['rmse'].append(perf['rmse'][1])
+    fold3cf['rmse'].append(perf['rmse'][2])
+    meanscf['rmse'].append(sum(perf['rmse'])/len(perf['rmse']))
+    
+    print_perf(perf)
+    
+
+"""
+part 14 of HW4.pdf
+
+"""
+CFKmodalgs = [KNNBasic(k=10, sim_options={'user_based': True, 'name':'MSD'}), KNNBasic(k=20, sim_options={'user_based': True,'name':'MSD'}), KNNBasic(k=30, sim_options={'user_based': True,'name':'MSD'}), KNNBasic(k=10, sim_options={'user_based': False,'name':'MSD'}), KNNBasic(k=20, sim_options={'user_based': False, 'name':'MSD'}), KNNBasic(k=30, sim_options={'user_based': False,'name':'MSD'})]
+fold1cfk = {'rmse':[]}
+fold2cfk = {'rmse':[]}
+fold3cfk = {'rmse':[]}
+meanscfk = {'rmse':[]}
+
+for alg in CFKmodalgs:
+    perf = evaluate(alg, data, measures=['RMSE'])
+    
+    fold1cfk['rmse'].append(perf['rmse'][0])
+    fold2cfk['rmse'].append(perf['rmse'][1])
+    fold3cfk['rmse'].append(perf['rmse'][2])
+    meanscfk['rmse'].append(sum(perf['rmse'])/len(perf['rmse']))
+    
+    print_perf(perf)
